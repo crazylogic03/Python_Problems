e36aa91425fd8e1a77eb23cc3eb4dd79af4f60e5
B Sai Thrishul <saithrishul2006@gmail.com>
2025-01-26 21:31:50 +0530

    Adding Python problems Daily

---
+++ printing DataTYpes.py
@@ -0,0 +1,11 @@
+''' Q.) Write a Python program that creates an integer variable age with the value 21 and a float variable price with the value 19.99. Print both variables and their data types using the type() function.'''
+
+# Output
+# You need to print value of variable age, datatype of age in first line and value of variable price, datatype of price in second line 
+
+# Answer:
+age = 21
+price = 19.99
+
+print(age,type(age))
+print(price,type(price))
