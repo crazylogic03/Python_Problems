1773c4302cd000c6d530fcc4984662d60df8f9e4
B Sai Thrishul <saithrishul2006@gmail.com>
2025-01-26 21:32:18 +0530

    Add files via upload

---
+++ Printing_Variables.py
@@ -0,0 +1,21 @@
+'''
+Write a Python program that assigns values to variables following the correct naming conventions. Create variables named firstName with value "Harsh", last_name with value "Gupta", _age with value 25, and print them in different lines.
+
+
+Output
+You need to print the values of variables named firstName, last_name, and _age with given values.
+
+
+Example
+Output:
+Harsh
+Gupta
+25'''
+
+# Answer:-
+firstname = "Harsh"
+last_name = "Gupta"
+_age = "25"
+print(firstname)
+print(last_name)
+print(_age)
